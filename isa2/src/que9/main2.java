6bff88acffdd06798def38d5ae566d32d85be90f
Efern <Efern@LAPTOP-S9HGLLE6>
2024-04-02 23:16:29 +0530

    commit

---
+++ isa2/src/que9/main2.java
@@ -0,0 +1,13 @@
+package que9;
+
+public class main2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Car myCar = new Car(80, "Toyota");
+
+	        // Call the drive method
+	        myCar.drive();
+	}
+
+}
