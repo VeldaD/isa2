97fdb738064cd52264bcb868517dcc9d5fb5043c
Efern <velda07dias@gmail.com>
2024-04-02 22:29:32 +0530

    commit

---
+++ isa2/src/que1/AddTwoIntegers.java
@@ -0,0 +1,30 @@
+package que1;
+
+import java.util.Scanner;
+
+public class AddTwoIntegers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner scanner = new Scanner(System.in);
+
+	        System.out.println("Enter the first integer:");
+	        int num1 = scanner.nextInt();
+
+	        System.out.println("Enter the second integer:");
+	        int num2 = scanner.nextInt();
+
+	        int sum = num1 + num2;
+
+	        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+
+}
+
+		public static int add(int num1, int num2) {
+			// TODO Auto-generated method stub
+			return 0;
+	
+	}
+
+}
