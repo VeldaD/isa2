a826a2e24877118139e4e4af90a21cecc384808a
Efern <velda07dias@gmail.com>
2024-04-03 21:17:43 +0530

    commit

---
+++ isa2/src/que11/Main.java
@@ -0,0 +1,26 @@
+package que11;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Main class
+				
+				        // Create an object of TeacherTeaches class
+				        TeacherTeaches teacher = new TeacherTeaches();
+
+				        // Set values for type and name
+				        teacher.type = "Mathematics";
+				        teacher.name = "John Doe";
+
+				        // Call methods
+				        teacher.Teaches();
+				        teacher.Subjects();
+				    }
+				
+
+
+
+	}
+
+
---
+++ isa2/src/que11/Teachers.java
@@ -0,0 +1,27 @@
+package que11;
+import java.util.Scanner;
+
+public class Teachers {
+	 protected String type;
+
+	    // Method
+	    public void Teaches() {
+	        System.out.println("This teacher teaches " + type);
+	    }
+	}
+
+	// Subclass
+	class TeacherTeaches extends Teachers {
+	    // New member variable
+	    String name;
+
+	    // Method
+	    public void Subjects() {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.print("Enter the subject taught by the teacher: ");
+	        String subject = scanner.nextLine();
+	        scanner.close();
+	        System.out.println("The teacher is teaching " + subject);
+	    }
+
+}
